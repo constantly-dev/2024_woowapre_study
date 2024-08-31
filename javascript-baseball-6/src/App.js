a81c64eda86fd22f91aa2ae1eaeb8be17778aba5
constantly-dev <rntqhdl10@naver.com>
2024-08-31 17:54:11 +0900

    refactor: validate 분리

--- javascript-baseball-6/src/App.js
+++ javascript-baseball-6/src/App.js
@@ -1,25 +1,7 @@
 import { Console, Random } from '@woowacourse/mission-utils';
+import validateInput from './validate.js';
 
 class App {
-  validateInput(input) {
-    // 숫자가 아닌 경우
-    if (!/^\d+$/.test(input)) {
-      throw new Error('입력한 값이 숫자가 아닙니다.');
-    }
-
-    // 숫자이지만 3자리가 아닌 경우
-    if (input.length !== 3) {
-      throw new Error('입력한 숫자는 반드시 3자리여야 합니다.');
-    }
-
-    // 숫자이고 3자리지만 중복된 숫자가 있는 경우
-    const digits = input.split('');
-    const uniqueDigits = new Set(digits);
-    if (uniqueDigits.size !== 3) {
-      throw new Error('입력한 숫자에 중복된 숫자가 있습니다.');
-    }
-  }
-
   async play() {
     await Console.print('숫자 야구 게임을 시작합니다.');
     // 컴퓨터가 제공하는 랜덤 숫자 3자리 얻기
@@ -51,7 +33,7 @@ class App {
       */
 
       try {
-        this.validateInput(input);
+        validateInput(input);
       } catch (error) {
         Console.print(error.message);
         break;
