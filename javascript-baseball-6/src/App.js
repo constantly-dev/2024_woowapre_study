1ca29e011baf368cebf9b4a00165832197ba3b6b
constantly-dev <rntqhdl10@naver.com>
2024-09-02 19:32:57 +0900

    refactor: play()에 있던 로직 메서드로 분리, BaseBallPolicy에 calculateCount/isGameEnd/validateUserNumbers 분리

--- javascript-baseball-6/src/App.js
+++ javascript-baseball-6/src/App.js
@@ -1,5 +1,3 @@
-import { Console, Random } from '@woowacourse/mission-utils';
-import UserNumbersValidator from './UserNumbersValidator.js';
 import UserRetryValidator from './UserRetryValidator.js';
 import Printer from './Printer.js';
 import Reader from './Reader.js';
@@ -7,77 +5,117 @@ import GameAsset from './GameAsset.js';
 import BaseBallPolicy from './BaseBallPolicy.js';
 
 class App {
-  #userNumbersValidator;
   #userRetryValidator;
   #gameAsset;
   #baseBallPolicy;
+  #computer;
+
   constructor() {
-    this.#userNumbersValidator = new UserNumbersValidator();
     this.#userRetryValidator = new UserRetryValidator();
     this.#gameAsset = new GameAsset();
     this.#baseBallPolicy = new BaseBallPolicy();
+    this.#computer = null;
   }
 
   #shouldRetry = (reKeyword) => {
-    this.#userRetryValidator.validate(reKeyword);
     if (reKeyword === '1') {
       return true;
-    } else if (reKeyword === '2') {
-      return false;
     }
+    return false;
   };
 
   async play() {
-    await Printer.print('숫자 야구 게임을 시작합니다.');
-    // 컴퓨터가 제공하는 랜덤 숫자 3자리 얻기
-    const computer = Random.pickUniqueNumbersInRange(1, 9, 3);
+    await this.#printGameStart();
+    await this.#init();
 
-    Printer.print(computer);
-    this.#gameAsset.init();
+    while (!this.#isGameEnd()) {
+      this.#initGameAsset();
+      const user = await this.#readUserInput();
 
-    while (this.#gameAsset.getStrikeCount() !== 3) {
-      this.#gameAsset.init();
-      const input = await Reader.read('숫자를 입력해주세요 : ');
+      this.#processStrike(user);
+      this.#processBall(user);
 
-      this.#userNumbersValidator.validate(input);
+      await this.#printRoundResult();
 
-      const user = input.split('').map(Number);
-
-      // 컴퓨터 랜덤 숫자와 유저 입력 숫자를 비교
-      // 스트라이크 개수 판단
-      for (let i = 0; i < 3; i++) {
-        if (this.#baseBallPolicy.isStrike(computer, user, i)) {
-          this.#gameAsset.increaseStrikeCount();
-        }
-      }
-
-      // 볼 개수 판단
-      for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-          if (this.#baseBallPolicy.isBall(computer, user, i, j)) {
-            this.#gameAsset.increaseBallCount();
-          }
-        }
-      }
-
-      await Printer.print(
-        `${this.#gameAsset.getBallCount()}볼 ${this.#gameAsset.getStrikeCount()}스트라이크`
-      );
-      if (this.#gameAsset.getStrikeCount() === 3) {
-        await Printer.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-
-        const reKeyword = await Reader.read(
-          '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'
-        );
+      if (this.#isGameEnd()) {
+        await this.#printFinalResult();
+        const reKeyword = await this.#readRetry();
 
         if (this.#shouldRetry(reKeyword)) {
           this.play();
-        } else {
-          break;
         }
+        break;
       }
     }
   }
+  #init = async () => {
+    this.#initGameAsset();
+    await this.#initComputerNumbers();
+  };
+
+  // 이후 게임에서 다른 gameAsset과 관련없는 초기화 로직을 추가할 경우,
+  // 확장성을 고려해 다시 메서드로 감싼다.
+  #initGameAsset = async () => {
+    this.#gameAsset.init();
+  };
+
+  #initComputerNumbers = async () => {
+    this.#computer = this.#baseBallPolicy.generateRandomNumbers();
+  };
+
+  #printGameStart = async () => {
+    await Printer.print('숫자 야구 게임을 시작합니다.');
+  };
+
+  #readUserInput = async () => {
+    const input = await Reader.read('숫자를 입력해주세요 : ');
+    this.#baseBallPolicy.validateUserNumbers(input);
+    return input.split('').map(Number);
+  };
+
+  #printRoundResult = async () => {
+    await Printer.print(
+      `${this.#gameAsset.getBallCount()}볼 ${this.#gameAsset.getStrikeCount()}스트라이크`
+    );
+  };
+
+  #printFinalResult = async () => {
+    await Printer.print(
+      `${BaseBallPolicy.GAME_NUMBER_LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료`
+    );
+  };
+
+  #readRetry = async () => {
+    const reKeyword = await Reader.read(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'
+    );
+    this.#userRetryValidator.validate(reKeyword);
+    return reKeyword;
+  };
+
+  #processStrike = (user) => {
+    const strikeCount = this.#baseBallPolicy.calculateStrikeCount(
+      this.#computer,
+      user
+    );
+    for (let i = 0; i < strikeCount; i++) {
+      this.#gameAsset.increaseStrikeCount();
+    }
+  };
+
+  #processBall = (user) => {
+    const ballCount = this.#baseBallPolicy.calculateBallCount(
+      this.#computer,
+      user
+    );
+    for (let i = 0; i < ballCount; i++) {
+      this.#gameAsset.increaseBallCount();
+    }
+  };
+
+  #isGameEnd = () => {
+    return this.#baseBallPolicy.isGameEnd(this.#gameAsset);
+  };
 }
 
 export default App;
--- javascript-baseball-6/src/BaseBallPolicy.js
+++ javascript-baseball-6/src/BaseBallPolicy.js
@@ -1,9 +1,42 @@
+import { Random } from '@woowacourse/mission-utils';
+import UserNumbersValidator from './UserNumbersValidator.js';
+
 export default class BaseBallPolicy {
-  isStrike = (computer, user, index) => {
-    return computer[index] === user[index];
+  static MIN_NUMBER = 1;
+  static MAX_NUMBER = 9;
+  static GAME_NUMBER_LENGTH = 3;
+
+  #userNumbersValidator = new UserNumbersValidator();
+
+  isStrike = (computer, userNumber, index) => {
+    return computer[index] === userNumber;
   };
 
-  isBall = (computer, user, i, j) => {
-    return computer[i] === user[j] && i !== j;
+  isBall = (computer, user, index, comNumber) => {
+    return user.includes(comNumber) && computer[index] !== user[index];
+  };
+
+  calculateStrikeCount = (computer, user) => {
+    return user.filter((userNumber, index) =>
+      this.isStrike(computer, userNumber, index)
+    ).length;
+  };
+
+  calculateBallCount = (computer, user) => {
+    return computer.filter((comNumber, index) =>
+      this.isBall(computer, user, index, comNumber)
+    ).length;
+  };
+
+  isGameEnd = (GameAsset) => {
+    return GameAsset.getStrikeCount() === BaseBallPolicy.GAME_NUMBER_LENGTH;
+  };
+
+  generateRandomNumbers = () => {
+    return Random.pickUniqueNumbersInRange(1, 9, 3);
+  };
+
+  validateUserNumbers = (input) => {
+    this.#userNumbersValidator.validate(input);
   };
 }
